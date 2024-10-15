59405b30075afb22b4593af06caf8e639689557c
MarcellGranat <granatcellmar98@gmail.com>
2024-10-15 11:37:11 +0200

    ensure order by percentile category

--- R/prep_concentration.R
+++ R/prep_concentration.R
@@ -20,13 +20,15 @@
 prep_concentration <- function(data, n_doctor, n_patient) {
   data |> 
     dplyr::select(n_doctor = {{ n_doctor }}, n_patient = {{ n_patient }}) |>
-    dplyr::mutate(patient_per_doctor = n_patient / n_doctor) |> 
-    dplyr::arrange(n_patient) |>
+    dplyr::mutate(
+      patient_per_doctor = n_patient / n_doctor,
+      percentile_category = rev(stringr::str_c("TOP", round((dplyr::row_number() / dplyr::n()) * 100))),
+      percentile_category = forcats::fct_inorder(percentile_category)
+    ) |> 
+    dplyr::arrange(percentile_category) |>
     dplyr::mutate(
       patient_per_doctor = round(patient_per_doctor, 0),
       percent_patient = n_patient / sum(n_patient, na.rm = TRUE),
       cumn_patient = cumsum(n_patient),
-      percentile_category = rev(stringr::str_c("TOP", round((dplyr::row_number() / dplyr::n()) * 100))),
-    ) |> 
-      dplyr::arrange(dplyr::desc(n_patient))
+    )
   }
