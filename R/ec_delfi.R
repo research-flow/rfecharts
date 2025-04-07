f1baf50963531dc2b9c5b4eef40288c96ffda841
Balint Mazzag <mazzag.balint@researchflow.hu>
2025-04-07 15:15:21 +0200

    modify ec delfi labels percentage tooltip nominal

--- R/ec_delfi.R
+++ R/ec_delfi.R
@@ -15,14 +15,13 @@
 #'
 #' @examples
 #' example_delfi |>
-#'   prep_delfi(ertek_name, count) |> 
+#'   prep_delfi(ertek_name, count) |>
 #'   ec_delfi(show_positives = "inclusive")
-#' 
+#'
 #' example_delfi |>
-#'   prep_delfi(ertek_name, count) |> 
+#'   prep_delfi(ertek_name, count) |>
 #'   ec_delfi(show_positives = "exclusive")
-#' 
-#' 
+#'
 ec_delfi <- function(data, colors = NULL, show_positives = "inclusive", nc = RFecharts::name_cleaner(), co = RFecharts::color(), as_json = shiny::isRunning(), theme = RFecharts::rf_echarts_theme) {
   if (is.null(theme)) theme <- \(x) x
 
@@ -39,23 +38,23 @@ ec_delfi <- function(data, colors = NULL, show_positives = "inclusive", nc = RFe
     }
   }
 
-  color_to_value <- data |> 
+  color_to_value <- data |>
     dplyr::ungroup() |>
     dplyr::distinct(value) |>
     dplyr::mutate(color = colors)
 
-  data <- data |> 
+  data <- data |>
     dplyr::left_join(color_to_value, by = "value")
 
   # select the top 50%
   if (show_positives == "exclusive") {
-    positives <- data |> 
+    positives <- data |>
       dplyr::slice_max(value, prop = .5)
 
     n_positives <- nrow(positives)
     rate_positives <- sum(positives$percent)
   } else if (show_positives == "inclusive") {
-    negatives <- data |> 
+    negatives <- data |>
       dplyr::slice_min(value, prop = .5)
 
     n_positives <- nrow(data) - nrow(negatives)
@@ -63,62 +62,84 @@ ec_delfi <- function(data, colors = NULL, show_positives = "inclusive", nc = RFe
   }
 
   if (show_positives == "inclusive" | show_positives == "exclusive") {
-    if (nc@language == "hun") { 
-      positive_title = stringr::str_c("Top ", n_positives, " százaléka: ", scales::percent(rate_positives))
+    if (nc@language == "hun") {
+      positive_title <- stringr::str_c("Top ", n_positives, " százaléka: ", scales::percent(rate_positives))
     } else if (nc@language == "eng") {
-      positive_title = stringr::str_c("Top ", n_positives, " positive values: ", scales::percent(rate_positives))
+      positive_title <- stringr::str_c("Top ", n_positives, " positive values: ", scales::percent(rate_positives))
     } else {
-      positive_title = stringr::str_c("Top ", n_positives, " százaléka: ", scales::percent(rate_positives))
+      positive_title <- stringr::str_c("Top ", n_positives, " százaléka: ", scales::percent(rate_positives))
     }
     e_show_positives <- function(e) {
-      e_mark_line(e, data = list(xAxis = 1 - rate_positives), 
-                   precision= 5,
-                   silent = TRUE,
-                   lineStyle = list(color = "black"),
-                   symbolSize = 0,
-                   label = list(fontSize = 14,
-                                fontWeight = "bold"),
-                   title = positive_title)
+      e_mark_line(e,
+        data = list(xAxis = 1 - rate_positives),
+        precision = 5,
+        silent = TRUE,
+        lineStyle = list(color = "black"),
+        symbolSize = 0,
+        label = list(
+          fontSize = 14,
+          fontWeight = "bold"
+        ),
+        title = positive_title
+      )
     }
   } else {
     e_show_positives <- function(e) e
   }
-  
-  data |> 
+
+  data |>
     dplyr::mutate(
       empty_x_col = "",
       value = forcats::fct_relabel(value, nc@prettify)
-    ) |> 
-    dplyr::group_by(value) |> 
+    ) |>
+    dplyr::group_by(value) |>
     e_charts(x = empty_x_col) |>
-    e_bar(percent, stack = "group", bind = n, 
-      label = list(show = TRUE,
-        formatter = "{b}"),
-        itemStdatale = list(
-          shadowColor = "black",
-          borderWidth = 2
-      )) |> 
-    e_show_positives() |> 
-    e_color(colors) |> 
+    e_bar(percent,
+      stack = "group", bind = n,
+      label = list(
+        show = TRUE,
+        formatter = htmlwidgets::JS("
+    function(params){
+      return (params.value[0] * 100).toFixed(0) + '%';
+    }
+  ")
+      ),
+      itemStdatale = list(
+        shadowColor = "black",
+        borderWidth = 2
+      )
+    ) |>
+    e_show_positives() |>
+    e_color(colors) |>
     e_legend(bottom = 21) |>
     e_add_nested("itemStdatale", color, borderColor) |>
-    e_x_axis(axisLine = list(show=FALSE),
-    axisTick = list(show=FALSE)) |>
-    e_y_axis(max = 1,
-    formatter = RFecharts::formatter("percent", language = nc@language)) |>
-    e_title(subtext = stringr::str_c("N = ",
-                  sum(data$n))
+    e_x_axis(
+      axisLine = list(show = FALSE),
+      axisTick = list(show = FALSE)
     ) |>
-    e_tooltip(trigger = "axis",
-    valueFormatter =
-      htmlwidgets::JS("
-    function(value){
-    return(Number(value).toLocaleString(undefined,{stdatale: 'percent', minimumFractionDigits:2}))
+    e_y_axis(
+      max = 1,
+      formatter = RFecharts::formatter("percent", language = nc@language)
+    ) |>
+    e_title(subtext = stringr::str_c(
+      "N = ",
+      sum(data$n)
+    )) |>
+    e_tooltip(
+      trigger = "axis",
+      formatter = htmlwidgets::JS("
+    function(params) {
+      let output = '';
+      params.forEach(function(p) {
+        let percent = (p.value[0] * 100).toFixed(1) + '%';
+        let count = p.data.bind;
+        output += p.marker + p.seriesName + ': ' + p.name + '<br/>';
+      });
+      return output;
     }
-                    ")
+  ")
     ) |>
     e_flip_coords() |>
-    theme() |> 
-    (\(x) if (as_json) echarts4r::e_inspect(x, json = T) else x) ()
-  
+    theme() |>
+    (\(x) if (as_json) echarts4r::e_inspect(x, json = T) else x)()
 }
